94f77acde3eaa9764594a0d1adb3dae05c61494b
batin <emirscofield327@gmail.com>
2026-07-06 12:37:20 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.20" apply false
-    id("com.google.devtools.ksp") version "2.2.20-1.0.29" apply false
+    id("com.google.devtools.ksp") version "2.2.20-2.0.3" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
     id("com.google.protobuf") version "0.9.4" apply false
 }
